1de9c09435e379d6350f345d82200f8b13c6cd5d
DavisVaughan <mvaugh15@uncc.edu>
2018-04-11 14:40:52 -0400

    Update auth tests

--- tests/testthat/helper-skip_auth.R
+++ tests/testthat/helper-skip_auth.R
@@ -1,9 +1,3 @@
-skip_on_local <- function() {
-  if(Sys.info()[["login"]] == "davisvaughan") {
-    testthat::skip("On local")
-  }
-}
-
 skip_if_no_auth <- function() {
   if (identical(Sys.getenv("RIINGO_TOKEN"), "")) {
     testthat::skip("No authentication available")
--- tests/testthat/test-auth.R
+++ tests/testthat/test-auth.R
@@ -1,9 +1,7 @@
 context("test-test_auth.R")
 
-test_that("Auth token can be set by options", {
-  skip_on_local() # my auth token is set in the environment
-  options(riingo_token = "token")
-  on.exit( {options("riingo_token" = NULL)} )
-
-  expect_equal(riingo_get_token(), "token")
+test_that("Auth token can be retrieved", {
+  skip_if_no_auth() # my auth token is set in the environment
+  token <- riingo_get_token()
+  expect_equal(class(token), "character") # Prevent showing token in log
 })

8bdecb11df2ec57efd998a162133b8a691e9b66c
Aaron Miles <amiles@MBP2018-010.local>
2019-08-01 14:34:50 -0400

    Update ticker command to allow NULL

--- R/news.R
+++ R/news.R
@@ -6,7 +6,7 @@
 #'
 #'
 #' @param ticker One or more tickers to download data for from Tiingo. Can be a
-#' stock, mutual fund, or ETF. A character vector.
+#' stock, mutual fund, or ETF. A character vector. If this parameter is left null, news sources that meet all other parameters returned
 #' @param start_date The first date to download data for.
 #' A character in the form YYYY-MM-DD, or a `Date` variable.
 #' @param end_date The last date to download data for.
@@ -17,7 +17,7 @@
 #' @param offset Pagination. Can potentially speed up queries, does not affect R output
 #'
 #' @details
-#' Returns a list of news sources
+#' Returns a data frame of news articles, descriptions, urls, sources
 #'
 #' @examples
 #'
@@ -37,7 +37,7 @@ riingo_news <- function(ticker=NULL, start_date = NULL, end_date = NULL, tags = 
    endpoint <- "news"
 
 # Assertions
-   assert_x_inherits(ticker, "ticker", class = "character")
+   assert_x_inherits(ticker, "ticker", c('NULL', "character"))
    assert_x_inherits_one_of(start_date, "start_date", c("NULL", "character", "Date", "POSIXct"))
    assert_x_inherits_one_of(end_date, "end_date", c("NULL", "character", "Date", "POSIXct"))
    assert_x_inherits_one_of(tags, "tags", c("NULL", "character"))
